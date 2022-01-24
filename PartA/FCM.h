b419f091b38505eec8b5a9dbb650d7d9b6f41455
Ana Luísa Ferreira <aluisaf@ua.pt>
2022-01-24 16:41:19 +0000

    more imports

--- PartA/FCM.h
+++ PartA/FCM.h
@@ -1,32 +1,37 @@
 #include <map>
 #include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
 
-class FCM {
-    public:
-        FCM();
-        FCM(uint8_t order);
-        
-        uint8_t getOrder();
-        uint64_t getContextCount(const std::string context);
-        std::map<char, uint64_t> getMapContext(const std::string context);
+class FCM
+{
+public:
+    FCM();
+    FCM(uint8_t order);
 
-        bool loadFCM(std::string filePath);
-        bool saveFCM(std::string filePath);
+    uint8_t getOrder();
+    uint64_t getContextCount(const std::string context);
+    std::map<char, uint64_t> getMapContext(const std::string context);
 
-        bool loadFile(std::string filePath);
-        bool printToFile(std::string filePath);
+    bool loadFCM(std::string filePath);
+    bool saveFCM(std::string filePath);
 
-        double modelEntropy();
-        
-    private:
-        uint8_t order;
-        uint64_t totalMatches;
+    bool loadFile(std::string filePath);
+    bool printToFile(std::string filePath);
 
-        std::map<std::string, std::map<char, uint64_t>> mapContext;
-        std::map<std::string, uint64_t> contextCount;
+    double modelEntropy();
 
-        //std::vector<std::string> getAllContexts(uint8_t order);
-        //std::vector<std::string> getAllContexts(std::vector<std::string> strings, uint8_t order);
+private:
+    uint8_t order;
+    uint64_t totalMatches;
 
-        //void initializeMap(std::map<std::string, std::map<char, uint32_t>>& mapContext, uint8_t order);
+    std::map<std::string, std::map<char, uint64_t>> mapContext;
+    std::map<std::string, uint64_t> contextCount;
+
+    //std::vector<std::string> getAllContexts(uint8_t order);
+    //std::vector<std::string> getAllContexts(std::vector<std::string> strings, uint8_t order);
+
+    //void initializeMap(std::map<std::string, std::map<char, uint32_t>>& mapContext, uint8_t order);
 };
