f46625b82d47d60429fb8692652785f0d6817121
joaodiogo10 <joaodiogohf@ua.pt>
2022-01-27 20:42:59 +0000

    findlang update

--- PartB2/findlang.cpp
+++ PartB2/findlang.cpp
@@ -4,27 +4,31 @@
 #include <iostream>
 #include <limits>
 
-bool listFiles(const std::string directory, std::vector<std::string> &fileVector);
+bool listFiles(const std::string directoryModels, const int order, std::vector<std::string> &fileVector);
 std::string getLanguageFromFile(const std::string fileName);
 
 int main(int argc, char *argv[]){
     std::string testFilePath; 
     std::string directoryModels;
     std::vector<std::string> modelFileList;
-
+    
     double alpha;
+    int order;
 
-    if(argc < 4) {
-        std::cout << "Usage: ./lang <modelsDirectoryPath> <testFilePath> <alpha>" << std::endl;
+    if(argc < 5 || std::stoi(argv[4]) < 0 ) {
+        std::cout << "Usage: ./findlang <modelsDirectoryPath> <testFilePath> <alpha> <order>" << std::endl;
         return -1;
     }
-    directoryModels = argv[1];
+    directoryModels = std::string(argv[1]) + "/Order_" + std::string(argv[4]);
     testFilePath = argv[2];
     alpha = std::stod(argv[3]);
+    order = std::stoi(argv[4]);
+
     std::cout << "Alpha: " << alpha << std::endl;
+    std::cout << "Order: " << order << std::endl;
 
 
-    if(!listFiles(directoryModels, modelFileList)) {
+    if(!listFiles(directoryModels, order, modelFileList)) {
         std::cout << "Invalid models directory path!" << std::endl;
         return -1;
     }
@@ -69,11 +73,12 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-bool listFiles(const std::string directory, std::vector<std::string> &fileVector) {
+bool listFiles(const std::string modelsDirectory, int order, std::vector<std::string> &fileVector) {
     DIR *dir;
     struct dirent *ent;
-    
-    if ((dir = opendir(directory.c_str())) != NULL) {
+    std::string directory; 
+
+    if ((dir = opendir(modelsDirectory.c_str())) != NULL) {
         /* print all the files and directories within directory */
         while ((ent = readdir (dir)) != NULL) {
             std::string fileName(ent->d_name);
